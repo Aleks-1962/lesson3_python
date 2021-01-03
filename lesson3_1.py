2762b0589a5a95ccdecbb9b4c55da52d3aab0af6
Aleks-1962 <ms_07_2012@mail.ru>
2021-01-03 09:44:20 +0400

    Добавил ДЗ-3 на 5 задачу потратил 1.5 дня

--- lesson3_1.py
+++ lesson3_1.py
@@ -1,1 +1,18 @@
+def f_func():
+    """ Проверяет введенные данные и возвращает значение частного """
+    try:
+        arg_1 = float(input('Введите делимое: '))
+        arg_2 = float(input('Введите делитель: '))
+    except ValueError:
+        print('Нужно вводить числовые данные')
+        return
+    if arg_2 != 0:
+        toch = int(input('Укажите точность расчета(количество знаков после запятой: '))
+        rezlt = float(arg_1/arg_2)
+        return round(rezlt, toch)
+    else:
+        print('Делитель должен быть не равным нулю')
 
+
+gg = f_func()
+print(gg)
--- lesson3_2.py
+++ lesson3_2.py
@@ -1,2 +1,12 @@
+def us_func(name, family, b_day, city, mail, tel):
+    return name + ' ' + family + ' ' + b_day + ' ' + city + ' ' + mail + ' ' + tel
 
 
+name = input('Ваше имя: ')
+family = input('Ваше фамилия: ')
+b_day = input('Дата рождения: ')
+city = input('Город: ')
+mail = input('Электронный адрес: ')
+tel = input('Телефон: ')
+gg = us_func(name, family, b_day, city, mail, tel)
+print('Введен пользователь ' + gg)
